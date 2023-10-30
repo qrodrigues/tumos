c6c5f6f92e464c565e5c15d5da256660c4434a4b
qrodrigues <quentin.rodrigues1912@gmail.com>
2023-10-30 23:32:46 +0100

    feat: encrypt words to prevent cheat

---
+++ config.js
@@ -0,0 +1,4 @@
+export default {
+    api_url: import.meta.env.VITE_API_URL || "http://localhost:3000",
+    api_secret_key: import.meta.env.API_SECRET || "my_secret_key"
+};
--- src/repository/word.js
+++ src/repository/word.js
@@ -1,34 +1,45 @@
-import axios from 'axios'
+import axios from "axios";
+import CryptoJS from "crypto-js";
+import config from '../../config'
 
-const url = import.meta.env.VITE_API_URL || 'http://localhost:3000'
+const secretKey = config.api_secret_key
+const url = config.api_url
+
+function decrypt(encryptedWord) {
+  return CryptoJS.AES.decrypt(encryptedWord, secretKey).toString(
+    CryptoJS.enc.Utf8
+  );
+}
 
 export default {
-    async isWord(word, size, firstLetter) {
-        const response = await axios.get(url + '/words/'+size+'/'+firstLetter);
-        return response.data.words.includes(word);
-    },
-    async getDailyWord() {
-        const response = await axios.get(url + '/words/daily');
-        return response.data.word;
-    },
-    async getHourlyWord() {
-        const response = await axios.get(url + '/words/hourly');
-        return response.data.word;
-    },
-    async getMinutelyWord() {
-        const response = await axios.get(url + '/words/minutely');
-        return response.data.word;
-    },
-    async getDailyDate() {
-        const response = await axios.get(url + '/words/daily/time');
-        return response.data.time;
-    },
-    async getHourlyDate() {
-        const response = await axios.get(url + '/words/hourly/time');
-        return response.data.time;
-    },
-    async getMinutelyDate() {
-        const response = await axios.get(url + '/words/minutely/time');
-        return response.data.time;
-    }
-}
+  async isWord(word, size, firstLetter) {
+    const response = await axios.get(
+      url + "/words/" + size + "/" + firstLetter
+    );
+    return response.data.words.includes(word);
+  },
+  async getDailyWord() {
+    const response = await axios.get(url + "/words/daily");
+    return decrypt(response.data.word)
+  },
+  async getHourlyWord() {
+    const response = await axios.get(url + "/words/hourly");
+    return decrypt(response.data.word)
+  },
+  async getMinutelyWord() {
+    const response = await axios.get(url + "/words/minutely");
+    return decrypt(response.data.word)
+  },
+  async getDailyDate() {
+    const response = await axios.get(url + "/words/daily/time");
+    return response.data.time;
+  },
+  async getHourlyDate() {
+    const response = await axios.get(url + "/words/hourly/time");
+    return response.data.time;
+  },
+  async getMinutelyDate() {
+    const response = await axios.get(url + "/words/minutely/time");
+    return response.data.time;
+  },
+};
